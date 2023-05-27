614b0c17997cc23c93f3374e6f7d7bbe3c6108c1
rosmildoLopes <[200~123316625+rosmildoLopes@users.noreply.github.com~>
2023-05-27 01:22:27 -0300

    Finish Hero Section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
-
   const [toggle, setToggle] = useState(false);
 
   return (
@@ -22,11 +21,30 @@ const Navbar = () => {
       </ul>
 
       <div className="sm:hidden flex flex-1 justify-end items-center text-red-800">
-        <img 
-          src={toggle ? close : menu}  
+        <img
+          src={toggle ? close : menu}
           alt="menu and close "
           className="w-[28px] h-[28px] object-contain"
-          onClick={() => setToggle((prev) => !prev)} />
+          onClick={() => setToggle((prev) => !prev)}
+        />
+
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } bg-black-gradient p-6 absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="flex flex-col list-none justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                className={`font-poppins font-normal cursor-pointer text-[16px] text-white 
+            ${index === navLinks.length - 1 ? "mr-0" : "mb-4"} `}
+                key={nav.id}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
